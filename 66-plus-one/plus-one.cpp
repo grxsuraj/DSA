07655621eaeebd2cdc5e1533db1267751a353520
Suraj Innus Pathan <surajyp2283@gmail.com>
2026-04-05 19:09:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (27.08%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         
-        int n = digits.size();
-        
-        // Traverse from last digit
-        for(int i = n - 1; i >= 0; i--) {
-            
-            if(digits[i] < 9) {
-                digits[i]++;   // add 1
-                return digits; // no carry needed
+        int n=digits.size();
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
             }
-            
-            digits[i] = 0; // carry case
+        digits[i]=0;
         }
-        
-        // If all digits were 9
         digits.insert(digits.begin(), 1);
         return digits;
     }
