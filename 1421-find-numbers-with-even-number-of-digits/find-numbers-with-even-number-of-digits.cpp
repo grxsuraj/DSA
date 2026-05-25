b2b654b47ac30a83d551b8ecce7fbe48f457ae4f
Suraj Innus Pathan <surajyp2283@gmail.com>
2026-05-25 22:26:57 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (87.94%) - LeetSync

---
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        
+        int count = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            int digits = 0;
+            int num = nums[i];
+
+            while(num > 0) {
+                digits++;
+                num = num / 10;
+            }
+
+            if(digits % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
