4940aac7a956236ba0d8cf915b0f9b355bce6d1f
Suraj Innus Pathan <surajyp2283@gmail.com>
2026-07-19 21:14:30 +0530

    Time: 1165 ms (93.92%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select  product_name, year,  price
 
-from Sales as S left join Product as P
+from Sales as S inner join Product as P
 on S.product_id= P.product_id;
