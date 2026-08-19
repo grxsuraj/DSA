786036a1cb2a20bb71f569ff88c549ab13625200
Suraj Innus Pathan <surajyp2283@gmail.com>
2026-08-19 18:22:04 +0530

    Time: 2 ms (15.81%) | Memory: 24 MB (19.63%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,22 +2,28 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
 
-        int j = -1;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == 0){
-                j = i;
+        int i=-1;
+
+        for(int j=0; j<nums.size()-1; j++){
+
+            if(nums[j]==0){
+                i=j;
                 break;
             }
         }
 
-        // Agar koi zero nahi mila, to kuch karne ki zaroorat nahi
-        if(j == -1) return;
+        if(i== -1){
+            return ;
+        }
 
-        for(int i = j+1; i < nums.size(); i++){   // <-- yahan "j" ki jagah "i" check karo
-            if(nums[i] != 0){
+        for(int j=i+1; j<nums.size(); j++ ){
+
+            if(nums[j]!= 0){
                 swap(nums[i], nums[j]);
-                j++;
+                i++;
             }
         }
+
+        
     }
 };
